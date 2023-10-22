0b1bfeed0a3afcc0e81ff449063cc5407f361410
Francisco Jefferson da Silva Rodrigues <jsilvardg@outlook.com>
2023-10-22 18:09:21 -0300

    factory storage.go

--- entity/storage.go
+++ entity/storage.go
@@ -1,7 +1,33 @@
 package entity
 
+import (
+	"errors"
+	"github.com/google/uuid"
+	"time"
+)
+
+var (
+	StorageLocationError = errors.New("invalid Location")
+)
+
 type Storage struct {
 	Base
 	location    string
 	description string
+}
+
+func NewStorage(location, description string) (Storage, error) {
+	if location == "" {
+		return Storage{}, StorageLocationError
+	}
+
+	return Storage{
+		Base: Base{
+			id:        uuid.New(),
+			createdAt: time.Now(),
+			updatedAt: time.Now(),
+		},
+		location:    location,
+		description: description,
+	}, nil
 }
